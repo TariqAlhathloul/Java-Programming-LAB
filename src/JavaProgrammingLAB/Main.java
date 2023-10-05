a35f4a56bfc90574dc33e8a763021521efb57d94
Tariq Hassan <tariq_assan@Tariq.local>
2023-10-05 18:37:43 +0300

    commit main

--- src/JavaProgrammingLAB/Main.java
+++ src/JavaProgrammingLAB/Main.java
@@ -3,9 +3,16 @@ package JavaProgrammingLAB;
 /*
  * PROJECT TITLE: Introduction to Java applications.
  *PURPOSE OF PROJECT: Practice Java programming language.
- *Start LAB DATE: week 4, 09/14/23
+ *Started LAB DATE: week 4, 09/14/23
  *HOW TO START THIS PROJECT:visit the following websites https://www.bluej.org/ or any IDE you Desire.
  */
+
+/*
+----------------------------------------------------------------------------!
+!                  هذا المستودع غير رسمي من الجامعة، فقط لغرض التدرب والتعلم.
+*this is unofficial repository and it's just created for practice purposes. !
+----------------------------------------------------------------------------!
+* */
 public class Main {
     public static void main(String[] args) {
         //computePower comp = new computePower();
