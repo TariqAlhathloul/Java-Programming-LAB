709561276c67b87b6303b7726e9463fbfeac412b
afa1425 <72097152+afa1425@users.noreply.github.com>
2023-10-27 12:38:05 +0300

    Update ArrayManipulation.java

--- src/JavaProgrammingLAB/ArrayManipulation.java
+++ src/JavaProgrammingLAB/ArrayManipulation.java
@@ -1,7 +1,6 @@
 package JavaProgrammingLAB;
 
 import java.util.Scanner;
-import java.util.random.RandomGenerator;
 
 /*
 |----------------------------------|
@@ -25,54 +24,91 @@ import java.util.random.RandomGenerator;
 */
 
 public class ArrayManipulation {
-    Scanner scan = new Scanner(System.in);
-    public void arrayManipulation() {
-        int arrSize, random;
-        do {
-            System.out.println("Enter The Array Length \"Must Be Between 10 and 20\" ");
-            arrSize = scan.nextInt();
-        } while (arrSize < 10 || arrSize > 20);
-        scan.close();
-
-        int[] orderedArray = new int[arrSize];
-        for (int i = 0; i < arrSize; i++) {
-            orderedArray[i] = i;
-        }
-        printArray(orderedArray);
-
-        int[] randomArr = new int[arrSize];
-        for(int i = 0; i < arrSize; i++) { // generate random values in the array less than 200
-            random = RandomGenerator.getDefault().nextInt(200)+1;
-            randomArr[i] = random;
-        }
-        printArray(randomArr);
-
-        int max = randomArr[0], min = randomArr[0], sum = 0;
-
-        for(int i = 0; i < randomArr.length; i++) {
-            if(randomArr[i] > max){ // find the max value in the array
-                max = randomArr[i];
+    public static void main(String[] args) {
+        //init Scanner
+        Scanner in=new Scanner(System.in);
+        
+        int x=0; //assign a placeholder value to x to avoid compile error
+        do{
+            try{
+                System.out.print("Enter a number between 10 and 20: "); //print question and store the entered number
+                x=in.nextInt();
+                while(x<10||x>20){ //when user's input is out of the 10-20 range
+                    System.out.print("\nThe number must be between 10 and 20\nEnter a number between 10 and 20: ");
+                    x=in.nextInt();
+                }
+            }catch(Exception e){ //when user's input is not an int
+                System.out.println("\nInput must be an integer between 10 and 20");
+                in.next();
             }
-            if(randomArr[i] < min){ // find the min value in the array
-                min = randomArr[i];
+        }while(x<10||x>20);
+        in.close();
+        
+        //create array with x elements
+        int[] arr=new int[x];
+        
+        //print state of array before init
+        System.out.println("\nState of array before intialization:");
+        for(int i:arr)
+            System.out.print(i+" ");
+        
+        //init array and print state
+        for(int i=0;i<arr.length;i++)
+            arr[i]=i;
+        System.out.println("\nState of array after intialization:");
+        for(int i:arr)
+            System.out.print(i+" ");
+        
+        //change array elements to random int
+        int min=0,max=200; //set range for random int
+        for(int i=0;i<arr.length;i++){
+            int randomInt=(int)Math.floor(Math.random()*(max-min+1)+min); //init a random int between 0 and 200
+            arr[i]=randomInt; //set arr[i] value to the random int
+        }
+        
+        //print state of array
+        System.out.println("\nState of array random filling:");
+        for(int i:arr)
+            System.out.print(i+" ");
+        
+        //finding the odd numbers
+        int[] oddPre=new int[x]; //create temporary array with length of x to store the odd numbers
+        int count=0;
+        for(int i:arr){
+            if(i%2!=0){ //test every element to see if it's odd
+                oddPre[count]=i; //set the value of oddPre[count] to the odd number
+                count++; //increment count to prepare for the next odd number
             }
-            sum += randomArr[i]; // sum of all the elements in the array
         }
-        System.out.println("Max = " + max + "\nMin = " + min + "\nSum = " + sum);
-        double average = sum / randomArr.length;
-        System.out.println("Average Of The Array: " + average);
-
-        System.out.print("Odds Values In The Array ");
-        for(int j : randomArr)
-            if(j % 2 != 0)
-                System.out.print(j + ", ");
-    }
-    public void printArray(int[] arr) {
-
-        System.out.print("Array = {");
-        for (int index : arr) {
-            System.out.print(index + ",");
-        }
-        System.out.println("}");
+        if(count!=0){
+            int[] odd=new int[count]; //create array for the odd numbers
+            System.arraycopy(oddPre, 0, odd, 0, count); //copy the odd numbers in oddPre to the newly created odd array in order to remove extra zeroes
+            System.out.println("\nOdd numbers:");
+            for (int i:odd)
+                System.out.print(i+" ");
+        }else //if count is still zero (no odd numbers found)
+            System.out.println("There are no odd numbers");
+        
+        //finding sum
+        int sum=0;
+        for(int i:arr)
+            sum+=i; //sum=sum+i
+        System.out.println("\n\nThe sum is: "+sum);
+        
+        //finding average
+        double avg=(double)sum/(double)x;
+        System.out.println("The average is: "+avg);
+        
+        //finding max element
+        int arrMax=0;
+        for(int i:arr)
+            arrMax=Math.max(arrMax, i);
+        System.out.println("The max is: "+arrMax);
+        
+        //finding min element
+        int arrMin=arrMax;
+        for(int i:arr)
+            arrMin=Math.min(arrMin, i);
+        System.out.println("The min is: "+arrMin);
     }
 }
